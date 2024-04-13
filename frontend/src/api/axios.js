910676e094718173a515b378a8c4b413f0f74b26
Adi-204 <adiboghawala@gmail.com>
2024-04-13 12:00:29 +0530

    Done

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:3000';
 
 const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
+    baseURL: process.env.VITE_URL,
     withCredentials: true
 });
 
